cab3b9966cf174839202437b48b54c35645c343c
Kaan Cömert <comert9806@gmail.com>
2022-11-12 20:33:16 +0300

    final

--- SnakePygame.py
+++ SnakePygame.py
@@ -17,6 +17,62 @@ down = (0,1)
 right = (1,0)
 left = (-1,0)
 
+class SNAKE:
+    def __init__(self):
+        self.positions = [((screen_width/2), (screen_height/2))]
+        self.lenght = 1
+        self.direction = random.choice([up,down,left,right])
+        self.color = snake_color
+        self.score = 0
+    def draw(self,surface):
+        for p in self.positions:
+            rect = pygame.Rect((p[0],p[1]),(grid_size,grid_size))
+            pygame.draw.rect(surface,self.color,rect)
+    def move(self):
+        current = self.positions[0]
+        x,y = self.direction
+        new = ((current[0] + (x * grid_size)), (current[1] + (y * grid_size)))
+
+        if new[0] in range (0,screen_width) and new[1] in range (0,screen_height) and not new in self.positions[2:]:
+            self.positions.insert(0,new)
+            if len(self.positions) > self.lenght:
+                self.positions.pop()
+        else:
+            self.reset()
+    def reset(self):
+        self.lenght = 1
+        self.positions = [((screen_width/2), (screen_height/2))]
+        self.direction = random.choice([up,down,left,right])
+        self.score = 0
+    
+    def handle_keys(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    self.turn(up)
+                elif event.key == pygame.K_s:
+                    self.turn(down)
+                elif event.key == pygame.K_d:
+                    self.turn(right)
+                elif event.key == pygame.K_a:
+                    self.turn(left)
+
+    def turn(self,direction):
+        if (direction[0] * -1, direction[1] * -1) == self.direction:
+            return
+        else:
+            self.direction = direction
+
+
+
+
+
+
+
 class FOOD: 
     def __init__(self):
         self.position = (0,0)
@@ -49,21 +105,28 @@ def drawGrid(surface):
 def main():
     pygame.init()
     screen = pygame.display.set_mode((screen_height,screen_width))
+    clock = pygame.time.Clock()
     font = pygame.font.SysFont("arial", 20, True, True)
     surface = pygame.Surface(screen.get_size())
     surface = surface.convert()
 
     food = FOOD()
+    snake = SNAKE()
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
+    while True: 
+        clock.tick(10)
+        snake.handle_keys()
+        snake.move()
         drawGrid(surface)
+        if snake.positions [0] == food.position:
+            snake.lenght += 1
+            snake.score += 1
+            food.random_position() 
         food.draw(surface)
+        snake.draw(surface)
         screen.blit(surface,(0,0))
+        score_text = font.render("Score {0}".format(snake.score), True,(0,0,0))
+        screen.blit(score_text,(10,10))
         pygame.display.update()
 
 main()
